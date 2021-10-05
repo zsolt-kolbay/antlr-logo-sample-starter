41dd7957dade3249b00d4b31606d66bd6d9418a9
zsolt-kolbay <59311329+zsolt-kolbay@users.noreply.github.com>
2021-10-05 09:50:31 +0200

    first commit
    Initial commit

---
+++ Turtle.Grammar/Logo.g4
@@ -0,0 +1,11 @@
+﻿grammar Logo;
+
+// Parser rules
+// Every identifier must start with a small letter.
+// Recursive rules are allowed.
+program: SOME_TOKEN;
+
+// Lexer rules
+// Every identifier must start with a capital letter.
+// Rules cannot directly reference each other.
+SOME_TOKEN: 'LOGO';
---
+++ Turtle.Grammar/LogoLanguageHelper.cs
@@ -0,0 +1,25 @@
+﻿using Antlr4.Runtime;
+
+namespace Turtle.Grammar
+{
+    public static class LogoLanguageHelper
+    {
+        public static void ParseAndExecuteLogoScript(string sourceCode, ITurtle turtle)
+        {
+            // create a stream from the source code
+            // pass that stream to the Lexer
+
+
+            // create a stream of tokens from the Lexer
+
+
+            // pass that stream to the Parser
+
+
+            // get the root node of the parsed syntax tree
+
+
+            // pass that root node to a Visitor object
+        }
+    }
+}
